a9de8bcfd2a27ca655ffbeff77cce4903dbb8bb0
kodemetrics <okaforwilson2@gmail.com>
2023-08-23 16:10:32 +0100

    update models.rs

--- src/models.rs
+++ src/models.rs
@@ -5,7 +5,7 @@ use serde::*;
 use validator::*;
 // use rocket::serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Validate, Queryable, Insertable,Selectable)]
+#[derive(Serialize, Deserialize, Validate, Queryable, Insertable, Selectable)]
 #[table_name = "posts"]
 pub struct Post {
     pub id: i32,
@@ -29,3 +29,11 @@ pub struct Person {
 //     pub body: String,
 //     pub published: bool,
 // }
+
+#[derive(Debug, Serialize, Deserialize, Validate)]
+pub struct User {
+    pub full_name: String,
+    pub username: String,
+    pub email: String,
+    pub password: String,
+}
